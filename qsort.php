d0e9041d6983122e9949e7d10d2195a54d00f81e
reese <reese.sexton01@gmail.com>
2021-01-28 16:54:31 +0000

    autoformatted in phpstorm

--- qsort.php
+++ qsort.php
@@ -1,20 +1,22 @@
 <?php
 
-function my_qsort(&$arr, $low, $high){
-    if($low < $high) {
-        
+function my_qsort(&$arr, $low, $high)
+{
+    if ($low < $high) {
+
         $pi = partition($arr, $low, $high);
 
-        my_qsort($arr, $low, $pi-1);
-        my_qsort($arr, $pi+1, $high);
+        my_qsort($arr, $low, $pi - 1);
+        my_qsort($arr, $pi + 1, $high);
     }
 }
 
-function partition(&$arr, $low, $high){
+function partition(&$arr, $low, $high)
+{
     $pivot = $arr[$high];
     $i = ($low - 1);
-    for($j=$low; $j <= $high; $j++){
-        if(strtoupper($arr[$j]) < strtoupper($pivot)){
+    for ($j = $low; $j <= $high; $j++) {
+        if (strtoupper($arr[$j]) < strtoupper($pivot)) {
             $i++;
             $temp = $arr[$i];
             $arr[$i] = $arr[$j];
@@ -22,7 +24,7 @@ function partition(&$arr, $low, $high){
         }
     }
     $temp = $arr[$i + 1];
-    $arr[$i + 1] =  $arr[$high];
+    $arr[$i + 1] = $arr[$high];
     $arr[$high] = $temp;
     return $i + 1;
 }
@@ -32,7 +34,7 @@ $test_arr = ['Jimmy', 9, 'Timothy', 8, 'William', 7, 'butts'];
 echo "BEFORE:\n";
 print_r($test_arr);
 
-my_qsort($test_arr, 0, count($test_arr)-1);
+my_qsort($test_arr, 0, count($test_arr) - 1);
 
 echo "AFTER:\n";
 print_r($test_arr);
